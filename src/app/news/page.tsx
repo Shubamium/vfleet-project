4f2b9ecff28119f9f000137d854b301b7377a99e
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-11-08 15:03:27 +0700

    Add news page

---
+++ src/app/news/page.tsx
@@ -0,0 +1,319 @@
+import { FaArrowRightLong } from "react-icons/fa6";
+import GeneralBG from "../component/generalBG/GeneralBG";
+import "./news.scss";
+export default function NewsPage() {
+  return (
+    <main id="page_news">
+      <GeneralBG />
+      <section id="news-heading" className="confine">
+        <img src="/gfx/icon_black.png" alt="" className="icon" />
+        <p className="common-sh">STAY UPDATED</p>
+        <div className="heading-line">
+          <h1 className="common-h">NEWS & INFO</h1>
+          <div className="line"></div>
+          <img src="/de/mini_plane-gold.png" alt="" className="plane" />
+        </div>
+        <p className="common-p">
+          We encourage you to support our talents on their streaming journeys,
+          however, VFleet asks you to consider donating to the nonprofit naval
+          and aerospace museums that inspired this project. If there is a museum
+          ship near you, we encourage not only a donation, but a visit as well!
+          Your support matters!
+        </p>
+      </section>
+
+      <section id="news-list">
+        <div className="category-select">
+          <div className="confine">
+            <button className="btn btn-category selected">ALL</button>
+            <button className="btn btn-category">TALENT</button>
+            <button className="btn btn-category">ANNOUNCEMENT</button>
+            <button className="btn btn-category">OTHER</button>
+          </div>
+        </div>
+
+        <div className="news-list confine">
+          <div className="news">
+            <div className="thumbnail">
+              <img src="" alt="" className="thumb" />
+            </div>
+            <div className="detail">
+              <div className="heading">
+                <div className="top-detail">
+                  <p className="category">CATEGORY</p>
+                  <p className="date"> 31 FEBRUARY 2024</p>
+                </div>
+                <h2 className="common-h s">POST TITLE HERE</h2>
+              </div>
+              <p className="common-p">
+                (Excerpt of the news) We encourage you to support our talents on
+                their streaming journeys, however, VFleet asks you to consider
+                donating to the nonprofit naval and aerospace museums that
+                inspired this project.
+              </p>
+
+              <div className="action">
+                <button className=" btn btn-outline">
+                  READ MORE
+                  <FaArrowRightLong />
+                </button>
+              </div>
+
+              <div className="decor-zig"></div>
+              <div className="decor-corner-l "></div>
+              <div className="decor-corner-r"></div>
+            </div>
+          </div>
+          <div className="news">
+            <div className="thumbnail">
+              <img src="" alt="" className="thumb" />
+            </div>
+            <div className="detail">
+              <div className="heading">
+                <div className="top-detail">
+                  <p className="category">CATEGORY</p>
+                  <p className="date"> 31 FEBRUARY 2024</p>
+                </div>
+                <h2 className="common-h s">POST TITLE HERE</h2>
+              </div>
+              <p className="common-p">
+                (Excerpt of the news) We encourage you to support our talents on
+                their streaming journeys, however, VFleet asks you to consider
+                donating to the nonprofit naval and aerospace museums that
+                inspired this project.
+              </p>
+
+              <div className="action">
+                <button className=" btn btn-outline">
+                  READ MORE
+                  <FaArrowRightLong />
+                </button>
+              </div>
+
+              <div className="decor-zig"></div>
+              <div className="decor-corner-l "></div>
+              <div className="decor-corner-r"></div>
+            </div>
+          </div>
+          <div className="news">
+            <div className="thumbnail">
+              <img src="" alt="" className="thumb" />
+            </div>
+            <div className="detail">
+              <div className="heading">
+                <div className="top-detail">
+                  <p className="category">CATEGORY</p>
+                  <p className="date"> 31 FEBRUARY 2024</p>
+                </div>
+                <h2 className="common-h s">POST TITLE HERE</h2>
+              </div>
+              <p className="common-p">
+                (Excerpt of the news) We encourage you to support our talents on
+                their streaming journeys, however, VFleet asks you to consider
+                donating to the nonprofit naval and aerospace museums that
+                inspired this project.
+              </p>
+
+              <div className="action">
+                <button className=" btn btn-outline">
+                  READ MORE
+                  <FaArrowRightLong />
+                </button>
+              </div>
+
+              <div className="decor-zig"></div>
+              <div className="decor-corner-l "></div>
+              <div className="decor-corner-r"></div>
+            </div>
+          </div>
+          <div className="news">
+            <div className="thumbnail">
+              <img src="" alt="" className="thumb" />
+            </div>
+            <div className="detail">
+              <div className="heading">
+                <div className="top-detail">
+                  <p className="category">CATEGORY</p>
+                  <p className="date"> 31 FEBRUARY 2024</p>
+                </div>
+                <h2 className="common-h s">POST TITLE HERE</h2>
+              </div>
+              <p className="common-p">
+                (Excerpt of the news) We encourage you to support our talents on
+                their streaming journeys, however, VFleet asks you to consider
+                donating to the nonprofit naval and aerospace museums that
+                inspired this project.
+              </p>
+
+              <div className="action">
+                <button className=" btn btn-outline">
+                  READ MORE
+                  <FaArrowRightLong />
+                </button>
+              </div>
+
+              <div className="decor-zig"></div>
+              <div className="decor-corner-l "></div>
+              <div className="decor-corner-r"></div>
+            </div>
+          </div>
+          <div className="news">
+            <div className="thumbnail">
+              <img src="" alt="" className="thumb" />
+            </div>
+            <div className="detail">
+              <div className="heading">
+                <div className="top-detail">
+                  <p className="category">CATEGORY</p>
+                  <p className="date"> 31 FEBRUARY 2024</p>
+                </div>
+                <h2 className="common-h s">POST TITLE HERE</h2>
+              </div>
+              <p className="common-p">
+                (Excerpt of the news) We encourage you to support our talents on
+                their streaming journeys, however, VFleet asks you to consider
+                donating to the nonprofit naval and aerospace museums that
+                inspired this project.
+              </p>
+
+              <div className="action">
+                <button className=" btn btn-outline">
+                  READ MORE
+                  <FaArrowRightLong />
+                </button>
+              </div>
+
+              <div className="decor-zig"></div>
+              <div className="decor-corner-l "></div>
+              <div className="decor-corner-r"></div>
+            </div>
+          </div>
+          <div className="news">
+            <div className="thumbnail">
+              <img src="" alt="" className="thumb" />
+            </div>
+            <div className="detail">
+              <div className="heading">
+                <div className="top-detail">
+                  <p className="category">CATEGORY</p>
+                  <p className="date"> 31 FEBRUARY 2024</p>
+                </div>
+                <h2 className="common-h s">POST TITLE HERE</h2>
+              </div>
+              <p className="common-p">
+                (Excerpt of the news) We encourage you to support our talents on
+                their streaming journeys, however, VFleet asks you to consider
+                donating to the nonprofit naval and aerospace museums that
+                inspired this project.
+              </p>
+
+              <div className="action">
+                <button className=" btn btn-outline">
+                  READ MORE
+                  <FaArrowRightLong />
+                </button>
+              </div>
+
+              <div className="decor-zig"></div>
+              <div className="decor-corner-l "></div>
+              <div className="decor-corner-r"></div>
+            </div>
+          </div>
+          <div className="news">
+            <div className="thumbnail">
+              <img src="" alt="" className="thumb" />
+            </div>
+            <div className="detail">
+              <div className="heading">
+                <div className="top-detail">
+                  <p className="category">CATEGORY</p>
+                  <p className="date"> 31 FEBRUARY 2024</p>
+                </div>
+                <h2 className="common-h s">POST TITLE HERE</h2>
+              </div>
+              <p className="common-p">
+                (Excerpt of the news) We encourage you to support our talents on
+                their streaming journeys, however, VFleet asks you to consider
+                donating to the nonprofit naval and aerospace museums that
+                inspired this project.
+              </p>
+
+              <div className="action">
+                <button className=" btn btn-outline">
+                  READ MORE
+                  <FaArrowRightLong />
+                </button>
+              </div>
+
+              <div className="decor-zig"></div>
+              <div className="decor-corner-l "></div>
+              <div className="decor-corner-r"></div>
+            </div>
+          </div>
+          <div className="news">
+            <div className="thumbnail">
+              <img src="" alt="" className="thumb" />
+            </div>
+            <div className="detail">
+              <div className="heading">
+                <div className="top-detail">
+                  <p className="category">CATEGORY</p>
+                  <p className="date"> 31 FEBRUARY 2024</p>
+                </div>
+                <h2 className="common-h s">POST TITLE HERE</h2>
+              </div>
+              <p className="common-p">
+                (Excerpt of the news) We encourage you to support our talents on
+                their streaming journeys, however, VFleet asks you to consider
+                donating to the nonprofit naval and aerospace museums that
+                inspired this project.
+              </p>
+
+              <div className="action">
+                <button className=" btn btn-outline">
+                  READ MORE
+                  <FaArrowRightLong />
+                </button>
+              </div>
+
+              <div className="decor-zig"></div>
+              <div className="decor-corner-l "></div>
+              <div className="decor-corner-r"></div>
+            </div>
+          </div>
+          <div className="news">
+            <div className="thumbnail">
+              <img src="" alt="" className="thumb" />
+            </div>
+            <div className="detail">
+              <div className="heading">
+                <div className="top-detail">
+                  <p className="category">CATEGORY</p>
+                  <p className="date"> 31 FEBRUARY 2024</p>
+                </div>
+                <h2 className="common-h s">POST TITLE HERE</h2>
+              </div>
+              <p className="common-p">
+                (Excerpt of the news) We encourage you to support our talents on
+                their streaming journeys, however, VFleet asks you to consider
+                donating to the nonprofit naval and aerospace museums that
+                inspired this project.
+              </p>
+
+              <div className="action">
+                <button className=" btn btn-outline">
+                  READ MORE
+                  <FaArrowRightLong />
+                </button>
+              </div>
+
+              <div className="decor-zig"></div>
+              <div className="decor-corner-l "></div>
+              <div className="decor-corner-r"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
