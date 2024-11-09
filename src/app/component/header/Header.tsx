694e4493c5047a87c02029e8f52c1c3c2e8cca2b
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-11-09 22:11:43 +0700

    Add header

--- src/app/component/header/Header.tsx
+++ src/app/component/header/Header.tsx
@@ -1,9 +1,13 @@
+"use client";
 import Link from "next/link";
 import "./header.scss";
 import { BiMenu } from "react-icons/bi";
 import { FaAnchor } from "react-icons/fa";
 import ScrollAnchor from "./scrollAnchor/ScrollAnchor";
+import { usePathname } from "next/navigation";
 export default function Header() {
+  const path = usePathname();
+  console.log(path);
   return (
     <header id="header">
       <div className="left">
@@ -13,25 +17,47 @@ export default function Header() {
       </div>
       <div className="right">
         <nav>
-          <Link href={"/"} className={`btn btn-nav active`}>
+          <Link
+            href={"/"}
+            className={`btn btn-nav ${path === "/" ? "active" : ""}`}
+          >
             <span>HOME</span>
           </Link>
-          <Link href={"/about"} className={`btn btn-nav`}>
+          <Link
+            href={"/about"}
+            className={`btn btn-nav ${path.includes("about") ? "active" : ""}`}
+          >
             <span>ABOUT</span>
           </Link>
-          <Link href={"/talents"} className={`btn btn-nav`}>
+          <Link
+            href={"/talents"}
+            className={`btn btn-nav ${
+              path.includes("talents") ? "active" : ""
+            }`}
+          >
             <span>TALENTS</span>
           </Link>
-          <Link href={"/donate"} className={`btn btn-nav`}>
+          <Link
+            href={"/donate"}
+            className={`btn btn-nav ${path.includes("donate") ? "active" : ""}`}
+          >
             <span>DONATE</span>
           </Link>
-          <Link href={"/news"} className={`btn btn-nav`}>
+          <Link
+            href={"/news"}
+            className={`btn btn-nav ${path.includes("news") ? "active" : ""}`}
+          >
             <span>NEWS</span>
           </Link>
           <a href={"#"} target="_blank" className={`btn btn-nav`}>
             <span>MERCH</span>
           </a>
-          <Link href={"/contact"} className={`btn btn-nav`}>
+          <Link
+            href={"/contact"}
+            className={`btn btn-nav ${
+              path.includes("contact") ? "active" : ""
+            }`}
+          >
             <span>CONTACT</span>
           </Link>
         </nav>
