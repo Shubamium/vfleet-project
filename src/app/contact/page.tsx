539bb00b9c02da3e9ad9a4b38359772573cadfee
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-11-10 12:35:58 +0700

    Add contact sent

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,42 @@
+"use client";
+import { FormEvent, useState } from "react";
 import GeneralBG from "../component/generalBG/GeneralBG";
 import GeneralHeading from "../component/generalHeading/GeneralHeading";
 import SocialList from "../component/socialList/SocialList";
 import "./contact.scss";
+
+import emailjs from "@emailjs/browser";
+import { LuLoader2 } from "react-icons/lu";
+
+emailjs.init({
+  publicKey: process.env.NEXT_PUBLIC_EMAILJS,
+  limitRate: {
+    // Set the limit rate for the application
+    id: "app",
+    // Allow 1 request per 10s
+    throttle: 10000,
+  },
+});
 export default function ContactPage() {
+  let [loading, setLoading] = useState(false);
+
+  const submit = async (e: FormEvent) => {
+    setLoading(true), e.preventDefault();
+    let data = await emailjs.sendForm(
+      "vfleetproject@gmail.com",
+      "template_705268e",
+      (e.target as HTMLButtonElement).form ?? "#contact-section"
+    );
+
+    console.log(data);
+    setLoading(false);
+  };
   return (
     <main id="page_contact">
+      <div className={`loading ${loading ? "loaded" : "hidden"}`}>
+        {loading ? <LuLoader2 /> : <></>}
+        <p> {loading ? "Loading . . ." : "Form Submitted!"}</p>
+      </div>
       <GeneralBG />
       <section id="top-section">
         <GeneralHeading
@@ -16,13 +48,13 @@ export default function ContactPage() {
         <SocialList />
       </section>
 
-      <form action="#" id="contact-section">
+      <form action="#" id="contact-section" onSubmit={submit}>
         <div className="panel">
           <div className="confine">
             <div className="form-group">
               <div className="fi">
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" placeholder="Your Name" />
+                <label htmlFor="from_name">Name</label>
+                <input type="text" name="from_name" placeholder="Your Name" />
               </div>
               <div className="fi">
                 <label htmlFor="email">Email:</label>
